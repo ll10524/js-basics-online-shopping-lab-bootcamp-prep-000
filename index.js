8c6f4f751b30c151146c38b9f76193f926f9eeed
Leo Tu <ll410119@gmail.com>
2018-01-07 04:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,7 @@ function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     let object = cart[i];
     if (object.hasOwnProperty(item)) {
-      cart = cart.splice(i, 1);
-      return cart;
+      return cart.splice(i, 1);
     }
   }
   console.log("That item is not in your cart.");
