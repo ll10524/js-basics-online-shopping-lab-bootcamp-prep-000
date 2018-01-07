d8609735f13f4a11e3153b671b244c30815ad8f4
Leo Tu <ll410119@gmail.com>
2018-01-07 04:37:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
-    let object = cart[0];
+    let object = cart[i];
     if (object.hasOwnProperty(item)) {
       cart = cart.splice(i, 1);
       return cart;
