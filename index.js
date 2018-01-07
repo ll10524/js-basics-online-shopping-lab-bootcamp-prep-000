7be435acb227530d6a0db04097b4f83671312e3e
Leo Tu <ll410119@gmail.com>
2018-01-07 04:41:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-    
+    console.log(`Your total cost is $${total()}, which will be charged to the card 83296759.`);
   }
 }
