601698d438e194527a18ca670ce203eb16a89cff
Leo Tu <ll410119@gmail.com>
2018-01-07 04:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,44 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ let object = {
+   [item]: Math.floor(Math.random() * 100)
+ }
+ cart.push(object);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  let word = "In your cart, you have";
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else if (cart.length === 1) {
+    let key = Object.keys(cart[0])[0];
+    word += `${key} at $${cart[0][key]}.`;
+    return word;
+  } else if (cart.length === 2) {
+    for (let i = 0; i < cart.length; i++) {
+      let key = Object.keys(cart[i])[0];
+      if (i != cart.length - 1) {
+        word += ` ${key} at $${cart[i][key]}`;
+      } else {
+        word += ` and ${key} at $${cart[i][key]}.`;
+      }
+      return word;
+    }
+  } else {
+    for (let i = 0; i < cart.length; i++) {
+      let key = Object.keys(cart[i])[0];
+      if (i != cart.length - 1) {
+        word += ` ${key} at $${cart[i][key]},`;
+      } else {
+        word += ` and ${key} at $${cart[i][key]}.`;
+      }
+      return word;
+    }
+  }
 }
 
 function total() {
